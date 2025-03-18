9c27f72501cda30b3ec984ddebfc72cd6b61ec0b
Charly00019 <charlottewalternerve@gmail.com>
2025-03-18 03:45:05 -0700

    chore: fixed Todo data type

--- internal/db/db.go
+++ internal/db/db.go
@@ -5,18 +5,19 @@ import (
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+
+	"github.com/Charly00019/Go-to-do-list/internal/models" // Import models package
 )
 
 var Database *gorm.DB
 
 func InitDB() {
 	var err error
-	Database, err = gorm.Open(sqlite.Open("todos.db"), &gorm.Config{})
+	Database, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
 	// Auto-migrate tables
-	var Todo int
-	Database.AutoMigrate(&Todo{})
+	Database.AutoMigrate(&models.Todo{}) // Use models.Todo
 }
--- internal/models/todo.go
+++ internal/models/todo.go
@@ -3,8 +3,7 @@ package models
 import "gorm.io/gorm"
 
 type Todo struct {
-	gorm.Model
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Completed   bool   `json:"completed"`
+    gorm.Model
+    Title  string `gorm:"not null"`
+    Status string `gorm:"not null"`
 }
